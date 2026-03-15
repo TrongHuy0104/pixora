69b73261775ef4ef9dd28485e2a2a60668417ab9
TrongHuy0104 <tronghuy0104@gmail.com>
2026-03-15 19:10:27 +0700

    feat: add edit tool ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/modules/hero";
 import Features from "@/modules/features";
 import Pricing from "@/modules/pricing";
+import Editor from "@/modules/editor";
 
 export default function Home() {
   return <div>
     <Hero />
     <Features />
     <Pricing />
+    <Editor />
   </div>;
 }
